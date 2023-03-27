92187f0809d9e2bf29a32bbd598b9f45027b7ac9
Stanisław Olejniczak <0stanislawolejniczak0@gmail.com>
2023-03-27 20:41:13 +0200

    Create Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import './styles/Home.css';
+
+function Home() {
+  return (
+    <main className="Home">
+      <h2 className="Home-heading">
+        Track films you’ve watched. <br /> Save those you want to see. <br />
+        Tell your friends what’s good.
+      </h2>
+      <button className="Home-getStarted" type="button">
+        Get started — it‘s free!
+      </button>
+      <p className="Home-subheading">
+        The social network for film lovers. Also available on 🍏 🤖
+      </p>
+    </main>
+  );
+}
+
+export default Home;
