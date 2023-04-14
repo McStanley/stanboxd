4338b667f55ce20946d3e5ec5626025c4b65490b
Stanisław Olejniczak <0stanislawolejniczak0@gmail.com>
2023-04-14 13:10:01 +0200

    Add search functionality to SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,20 @@
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
+import { useLocation, useMatch, useNavigate } from 'react-router-dom';
 import SearchIcon from '../assets/search.svg';
 import './styles/SearchBar.css';
 
 function SearchBar() {
   const [value, setValue] = useState('');
   const location = useLocation();
+  const match = useMatch('/search/:category/:query');
+  const navigate = useNavigate();
+  const inputRef = useRef(null);
+  const submitRef = useRef(null);
 
   useEffect(() => {
-    setValue('');
-  }, [location.pathname]);
+    const newValue = match?.params.query || '';
+    setValue(newValue);
+  }, [location]);
 
   const handleChange = (e) => {
     const newValue = e.target.value;
@@ -18,6 +23,11 @@ function SearchBar() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    inputRef.current.blur();
+    submitRef.current.blur();
+
+    navigate(`/search/films/${encodeURIComponent(value)}`);
   };
 
   return (
@@ -27,8 +37,9 @@ function SearchBar() {
         type="text"
         value={value}
         onChange={handleChange}
+        ref={inputRef}
       />
-      <button className="SearchBar-submit" type="submit">
+      <button className="SearchBar-submit" type="submit" ref={submitRef}>
         <img className="SearchBar-icon" src={SearchIcon} alt="Search" />
       </button>
     </form>
