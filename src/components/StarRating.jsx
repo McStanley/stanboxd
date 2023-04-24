a9cb598e236755d96902e3c800fba4be1496cd18
Stanisław Olejniczak <0stanislawolejniczak0@gmail.com>
2023-04-24 19:06:21 +0200

    Create StarRating component

---
+++ src/components/StarRating.jsx
@@ -0,0 +1,53 @@
+import Rating from '@mui/material/Rating';
+import PropTypes from 'prop-types';
+import './styles/StarRating.css';
+
+const sxStyles = {
+  '& .MuiRating-iconFilled': {
+    color: '#00e054',
+  },
+  '& .MuiRating-iconHover': {
+    color: '#40bcf4',
+  },
+};
+
+function StarRating({ value, setValue }) {
+  const isInteractive = !!setValue;
+
+  const handleChange = (_e, newValue) => newValue && setValue(newValue);
+
+  return (
+    <div className="StarRating">
+      {isInteractive && value && (
+        <button
+          className="StarRating-delete"
+          type="button"
+          onClick={() => setValue(null)}
+        >
+          ×
+        </button>
+      )}
+
+      <Rating
+        precision={0.5}
+        size={isInteractive ? 'large' : 'small'}
+        sx={sxStyles}
+        value={value}
+        readOnly={!isInteractive}
+        onChange={isInteractive ? handleChange : null}
+      />
+    </div>
+  );
+}
+
+StarRating.propTypes = {
+  value: PropTypes.number,
+  setValue: PropTypes.func,
+};
+
+StarRating.defaultProps = {
+  value: null,
+  setValue: null,
+};
+
+export default StarRating;
