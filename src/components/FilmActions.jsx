6e92be594495f98ffe18b3a197241d61c7ecfa69
Stanisław Olejniczak <0stanislawolejniczak0@gmail.com>
2023-04-25 16:48:58 +0200

    Implement film rating functionality

--- src/components/FilmActions.jsx
+++ src/components/FilmActions.jsx
@@ -1,17 +1,71 @@
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  deleteField,
+  getDoc,
+  query,
+  serverTimestamp,
+  updateDoc,
+  where,
+} from 'firebase/firestore';
 import PropTypes from 'prop-types';
+import { db } from '../firebase';
 import { useUserContext } from '../contexts/UserContext';
+import StarRating from './StarRating';
 import Share from './Share';
 import './styles/FilmActions.css';
+
+const reviewsRef = collection(db, 'reviews');
 
 function FilmActions({ filmId, openSignIn }) {
   const [userData, userLoading] = useUserContext();
 
+  const q = userData
+    ? query(
+        reviewsRef,
+        where('film_id', '==', filmId),
+        where('user_id', '==', userData.uid)
+      )
+    : null;
+
+  const [reviewData, reviewLoading, , reviewSnapshot] = useCollectionData(q);
+
+  const updateRating = async (value) => {
+    if (reviewData.length) {
+      const reviewRef = reviewSnapshot.docs[0].ref;
+
+      await updateDoc(reviewRef, {
+        rating: value || deleteField(),
+        updatedAt: serverTimestamp(),
+      });
+
+      const updatedDoc = await getDoc(reviewRef);
+
+      if (!updatedDoc.get('rating') && !updatedDoc.get('text')) {
+        await deleteDoc(reviewRef);
+      }
+    } else {
+      await addDoc(reviewsRef, {
+        user_id: userData.uid,
+        film_id: filmId,
+        rating: value,
+        createdAt: serverTimestamp(),
+      });
+    }
+  };
+
+  const isLoading = userLoading || reviewLoading;
+  const review = reviewData?.[0];
+
   return (
     <section className="FilmActions">
-      {userLoading && (
+      {isLoading && (
         <div className="FilmActions-loading">Loading user data...</div>
       )}
-      {!userLoading && !userData && (
+
+      {!isLoading && !userData && (
         <button
           className="FilmActions-signIn"
           type="button"
@@ -20,14 +74,19 @@ function FilmActions({ filmId, openSignIn }) {
           Sign in to log, rate or review
         </button>
       )}
-      {!userLoading && userData && (
+
+      {!isLoading && userData && (
         <>
-          <div className="FilmActions-rate">Rate</div>
+          <div className="FilmActions-rate">
+            <p>{review?.rating ? 'Rated' : 'Rate'}</p>
+            <StarRating value={review?.rating} setValue={updateRating} />
+          </div>
           <button className="FilmActions-review" type="button">
             Review or log...
           </button>
         </>
       )}
+
       <Share url={`https://stanboxd.vercel.app/film/${filmId}`} />
     </section>
   );
