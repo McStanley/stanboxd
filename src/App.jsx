c7097f19bd1a8c694a63266dd6291d91270f8748
Stanisław Olejniczak <0stanislawolejniczak0@gmail.com>
2023-04-27 13:21:13 +0200

    Refactor Overlay component to use createPortal

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div id="App" className="App">
       <UserProvider>
         <Header
           openSignUp={toggleShowSignUp}
--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,8 +1,12 @@
+import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import './styles/Overlay.css';
 
 function Overlay({ children }) {
-  return <div className="Overlay">{children}</div>;
+  return createPortal(
+    <div className="Overlay">{children}</div>,
+    document.getElementById('App')
+  );
 }
 
 Overlay.propTypes = {
