e982b7b9e28dd9a4dbf983ff8be853f731c795bc
Stanisław Olejniczak <0stanislawolejniczak0@gmail.com>
2023-04-28 17:14:36 +0200

    Display reviews on Film page

--- src/pages/Film.jsx
+++ src/pages/Film.jsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { collection, orderBy, query, where } from 'firebase/firestore';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 import PropTypes from 'prop-types';
+import { db } from '../firebase';
 import Loading from '../components/Loading';
 import Background from '../components/Background';
 import Poster from '../components/Poster';
 import FilmActions from '../components/FilmActions';
+import FilmPageReview from '../components/FilmPageReview';
 import './styles/Film.css';
+
+const reviewsRef = collection(db, 'reviews');
 
 function Film({ openSignIn }) {
   const [filmData, setFilmData] = useState(null);
@@ -25,6 +31,14 @@ function Film({ openSignIn }) {
     })();
   }, []);
 
+  const reviewsQuery = query(
+    reviewsRef,
+    where('film_id', '==', +id),
+    orderBy('createdAt', 'desc')
+  );
+
+  const [reviewsData, reviewsLoading] = useCollectionData(reviewsQuery);
+
   if (!filmData) return <Loading />;
 
   const releaseYear = filmData.release_date
@@ -38,6 +52,15 @@ function Film({ openSignIn }) {
   const voteAverage = (
     Math.round((filmData.vote_average / 2) * 10) / 10
   ).toFixed(1);
+
+  const reviewElements = reviewsData?.map((entry) => (
+    <FilmPageReview
+      userId={entry.user_id}
+      rating={entry.rating}
+      text={entry.text}
+      key={entry.user_id}
+    />
+  ));
 
   const styles = {
     paddingTop: filmData.backdrop_path ? 380 : 20,
@@ -88,6 +111,20 @@ function Film({ openSignIn }) {
               <div className="Film-ratingScore">★ {voteAverage}</div>
             </section>
           </div>
+        </section>
+        <section className="Film-reviews">
+          <div className="Film-reviewsHeader">Reviews</div>
+          {reviewsLoading && (
+            <p className="Film-reviewsInfo">Loading reviews...</p>
+          )}
+
+          {!reviewsLoading && !reviewsData.length && (
+            <p className="Film-reviewsInfo">
+              There are no reviews for this movie yet.
+            </p>
+          )}
+
+          {!reviewsLoading && !!reviewsData.length && reviewElements}
         </section>
       </main>
     </div>
