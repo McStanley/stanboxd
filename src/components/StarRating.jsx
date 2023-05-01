abf13c70a83d1161821ebd8d1362576d8649ca2a
Stanisław Olejniczak <0stanislawolejniczak0@gmail.com>
2023-05-01 13:36:06 +0200

    Hide StarRating empty stars in read-only mode

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -2,19 +2,22 @@ import Rating from '@mui/material/Rating';
 import PropTypes from 'prop-types';
 import './styles/StarRating.css';
 
-const sxStyles = {
-  '& .MuiRating-iconFilled': {
-    color: '#00e054',
-  },
-  '& .MuiRating-iconHover': {
-    color: '#40bcf4',
-  },
-};
-
 function StarRating({ value, setValue }) {
   const isInteractive = !!setValue;
 
   const handleChange = (_e, newValue) => newValue && setValue(newValue);
+
+  const sxStyles = {
+    '& .MuiRating-iconEmpty': {
+      visibility: isInteractive ? 'visible' : 'hidden',
+    },
+    '& .MuiRating-iconFilled': {
+      color: '#00e054',
+    },
+    '& .MuiRating-iconHover': {
+      color: '#40bcf4',
+    },
+  };
 
   return (
     <div className="StarRating">
